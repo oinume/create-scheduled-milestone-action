ddfb620b106c0ab5eeb22e40b7bcbc04b2c0846e
Kazuhiro Oinuma <oinume@gmail.com>
2020-04-11 17:21:07 +0900

    Structure app

--- main.go
+++ main.go
@@ -1,14 +1,44 @@
 package main
 
 import (
-	"fmt"
+	"errors"
 	"os"
+	"time"
 )
 
 func main() {
-	myInput := os.Getenv("INPUT_MYINPUT")
+	//myInput := os.Getenv("INPUT_MYINPUT")
+	//
+	//output := fmt.Sprintf("Hello %s", myInput)
+	//
+	//fmt.Println(fmt.Sprintf(`::set-output name=myOutput::%s`, output))
+	a := &app{
+		// TODO: github client
+	}
+	title := os.Getenv("INPUT_TITLE")
+	description := os.Getenv("INPUT_DESCRIPTION")
+	dueOn := os.Getenv("INPUT_DUE_ON")
+	if err := a.run(title, description, dueOn); err != nil {
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
 
-	output := fmt.Sprintf("Hello %s", myInput)
+type app struct {}
 
-	fmt.Println(fmt.Sprintf(`::set-output name=myOutput::%s`, output))
+func (c *app) run(title, description string, dueOn string) error {
+	if title == "" {
+		return errors.New("'title' is required")
+	}
+
+	_, err := c.validateDueOn(dueOn)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *app) validateDueOn(dueOn string) (time.Time, error) {
+	return time.Time{}, nil
 }
